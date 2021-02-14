8c8573503f40d6b84adac1cffe9c9aa699a5bb53
invierno <1354002980@qq.com>
2021-02-14 23:32:57 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  hi
+  <First/>
+</div>
+</template>
+
+<script>
+import First from './components/First.vue'
+
+export default {
+  name: 'App',
+  components: {
+    First:First
+  }
+}
+</script>
