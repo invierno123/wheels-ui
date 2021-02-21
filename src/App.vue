5c3c351ffbc53be4557428a1895194ebbe954975
invierno <1354002980@qq.com>
2021-02-21 23:46:40 +0800

    PC 默认显示边栏，手机默认不显示边栏

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@
 </template>
 
 <script lang="ts">
-import {ref , provide} from 'vue';
+import {ref, provide} from 'vue';
 
 export default {
   name: 'App',
   setup() {
-    const asideVisible = ref(false);
-    provide('aside',asideVisible);
+    const width = document.documentElement.clientWidth;
+    const asideVisible = ref(width > 500);
+    provide('aside', asideVisible);
   }
 
 };
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -41,11 +41,6 @@ export default {
 aside{
   background: lightblue;
   width: 150px;
-
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
   > h2{
     margin-bottom: 4px;
   }
@@ -53,6 +48,13 @@ aside{
     > li{
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
