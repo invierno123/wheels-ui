f0c600b9589b00bb4225af2ba9c1dc2189278af9
invierno <1354002980@qq.com>
2021-03-09 23:23:01 +0800

    创建 Dialog 组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>不太</header>
+    <main>
+      <p>的方法</p>
+      <p>长度</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components:{Button}
+}
+</script>
