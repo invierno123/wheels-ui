735a649ea957aec3b5f390e3002ff969a028e6e8
invierno <1354002980@qq.com>
2021-03-09 23:48:27 +0800

    添加组件样式

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,17 @@
 <template>
-<div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>不太</header>
+<div class="wheel-dialog-overlay"></div>
+  <div class="wheel-dialog-wrapper">
+    <div class="wheel-dialog">
+    <header>不太<span class="wheel-dialog-close"></span></header>
     <main>
       <p>的方法</p>
       <p>长度</p>
     </main>
     <footer>
-      <Button>OK</Button>
+      <Button level="main">OK</Button>
       <Button>Cancel</Button>
     </footer>
+  </div>
   </div>
 </template>
 
@@ -20,3 +22,72 @@ export default {
   components:{Button}
 }
 </script>
+<style lang="scss">
+$radius:4px;
+$border-color:#d9d9d9;
+.wheel-dialog{
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade-out(black,0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  >header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  >main {
+    padding: 12px 16px;
+  }
+  >footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+  }
+</style>
