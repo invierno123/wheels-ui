986a4ea6f60a52e2b5ff550674ccce71f0bc7c9e
invierno <1354002980@qq.com>
2021-03-10 13:36:04 +0800

    Dialog 实现关闭功能

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,43 +1,76 @@
 <template>
-<template v-if="visible">
-<div class="wheel-dialog-overlay"></div>
-  <div class="wheel-dialog-wrapper">
-    <div class="wheel-dialog">
-    <header>不太<span class="wheel-dialog-close"></span></header>
-    <main>
-      <p>的方法</p>
-      <p>长度</p>
-    </main>
-    <footer>
-      <Button level="main">OK</Button>
-      <Button>Cancel</Button>
-    </footer>
-  </div>
-  </div>
-</template>
+  <template v-if="visible">
+    <div class="wheel-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="wheel-dialog-wrapper">
+      <div class="wheel-dialog">
+        <header>不太<span @click="close" class="wheel-dialog-close"></span></header>
+        <main>
+          <p>的方法</p>
+          <p>长度</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
 </template>
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
-  props:{
-    visible:{
-      type:Boolean,
-      default:false,
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function
     }
   },
-  components:{Button}
-}
+  components: {Button},
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+    return {
+      close, onClickOverlay, ok, cancel
+    };
+  }
+};
 </script>
 <style lang="scss">
-$radius:4px;
-$border-color:#d9d9d9;
-.wheel-dialog{
+$radius: 4px;
+$border-color: #d9d9d9;
+.wheel-dialog {
   background: white;
   border-radius: $radius;
-  box-shadow: 0 0 3px fade-out(black,0.5);
+  box-shadow: 0 0 3px fade-out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -47,6 +80,7 @@ $border-color:#d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -54,7 +88,8 @@ $border-color:#d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -62,14 +97,17 @@ $border-color:#d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
@@ -96,5 +134,5 @@ $border-color:#d9d9d9;
       transform: translate(-50%, -50%) rotate(45deg);
     }
   }
-  }
+}
 </style>
