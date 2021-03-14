5ac055ca6baf15ebad4de554d3f7705d87eb0be7
invierno <1354002980@qq.com>
2021-03-15 01:07:48 +0800

    内容切换，显示被选中的内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Tabs示例
   </div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="nav1">111</Tab>
     <Tab title="nav2">222</Tab>
   </Tabs>
@@ -11,10 +11,18 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
+
 export default {
   components: {
     Tabs,
     Tab
+  },
+  setup() {
+    const x = ref('nav1');
+    return{
+      x
+    }
   }
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="wheel-tabs">
+    <div class="wheel-tabs-nav">
+      <div class="wheel-tabs-nav-item "
+           v-for="(t,index) in titles"
+           :class="{selected:t===selected}"
+           @click="select(t)"
+           :key="index">{{ t }}
+      </div>
+    </div>
+    <div class="wheel-tabs-content">
+      <component class="wheel-tabs-content-item" :is="current" :key="selected"/>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue';
+import {computed} from 'vue'
 
 export default {
+  props: {
+    selected: {type: String}
+  },
   setup(props, context) {
     const defaults = context.slots.default();
 
@@ -16,13 +29,52 @@ export default {
         throw new Error('Tabs 子标签非 Tab');
       }
     });
+    const current = computed(()=>{
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });//filter返回值是数组
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
 
     return {
-      defaults,titles
+      defaults, titles, current, select
     };
   }
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
