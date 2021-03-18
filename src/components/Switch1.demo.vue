ab38cfb73642c7083be26321db4292ded4cda2cb
invierno <1354002980@qq.com>
2021-03-18 20:51:07 +0800

    展示源代码

--- src/components/Switch1.demo.vue
+++ src/components/Switch1.demo.vue
@@ -1,18 +1,21 @@
+<demo>
+常规用法
+</demo>
 <template>
   <Switch v-model:value="bool"/>
 </template>
 
 <script lang="ts">
 
-import Button from '../lib/Button.vue';
+
 import {ref} from 'vue';
 import Switch from '../lib/Switch.vue';
 
 export default {
   components: {
     Switch,
-    Button
   },
+
 
   setup() {
     const bool = ref(false);
--- src/components/Switch2.demo.vue
+++ src/components/Switch2.demo.vue
@@ -1,17 +1,18 @@
+<demo>
+支持 disabled
+</demo>
 <template>
   <Switch v-model:value="bool" disabled/>
 </template>
 
 <script lang="ts">
 
-import Button from '../lib/Button.vue';
 import {ref} from 'vue';
 import Switch from '../lib/Switch.vue';
 
 export default {
   components: {
     Switch,
-    Button
   },
 
   setup() {
