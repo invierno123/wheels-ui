3d7356f209aa71e8de360f6608239722922611ec
invierno <1354002980@qq.com>
2021-03-20 00:40:50 +0800

    解决 build 后产生的 bug

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,28 +1,16 @@
 <template>
-  <article class="markdown-body" v-html="content">
-  </article>
+  <article class="markdown-body" v-html="content"></article>
 </template>
 <script lang="ts">
 
-import {ref} from 'vue';
-
 export default {
   props: {
-    path: {
+    content: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
-  setup(props) {
-    const content = ref<string>('');
-    import(props.path).then(result => {//异步
-      content.value = result.default;
-    });
-    return {
-      content
-    };
-  }
+
+
 };
 </script>
-
-
